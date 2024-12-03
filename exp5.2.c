b3e2dda3a0256a18973e8e47eefce6f572883610
Patade-Soham <sohampatade30@eng.rizvi.edu.in>
2024-12-03 13:13:00 +0530

    Update exp5.2.c

--- exp5.2.c
+++ exp5.2.c
@@ -2,22 +2,39 @@
 UIN:241P004
 Title: WAP to find the factorial of a number using recursive function. */
 #include <stdio.h>
-int calcRecursiveFact (int x) {
-if (x==0||x==1) {
-return 1;
+
+
+int factorial(int n) {
+
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+
+        return n * factorial(n - 1);
+    }
 }
-return x*calcRecursiveFact(x-1); }
-int calcProduct (int x) {
-if (x==1) {
-return 1;
-}
-return x*calcProduct(x-1); }
+
 int main() {
-int num=5, product=1;
-product=calcProduct(num); printf("%d", product);
-return 0;
+    int num;
+
+    
+    printf("Enter a number to find its factorial: ");
+    scanf("%d", &num);
+
+   
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        
+        printf("The factorial of %d is %d\n", num, factorial(num));
+    }
+
+    return 0;
 }
 /*OUTPUT
-120
-=== Code Execution Successful ===
+Enter a number to find its factorial: 5
+The factorial of 5 is 120
+
+Process returned 0 (0x0)   execution time : 2.067 s
+Press any key to continue.
 */
